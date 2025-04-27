45ebb64ae5e2c3d3348fceb5c9af6f1f2503f9b8
hliebovkyrylo <kirilglebov06@gmail.com>
2025-04-27 13:58:22 +0300

    fix

--- src/otp/otp.controller.ts
+++ src/otp/otp.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { OtpService } from './otp.service';
 
 @Controller('otp')
 export class OtpController {
   constructor(private readonly otpService: OtpService) {}
+
+  @Post('send')
+  @UseGuards(AuthGuard)
+  async sendOtp(@Req() req): Promise<void> {
+    const user = req.user;
+    await this.otpService.generateAndSendOtp(req.user);
+  }
 }
--- src/otp/otp.module.ts
+++ src/otp/otp.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OtpService } from './otp.service';
 import { OtpController } from './otp.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
+  imports: [PrismaModule, MailModule],
   controllers: [OtpController],
   providers: [OtpService],
 })
