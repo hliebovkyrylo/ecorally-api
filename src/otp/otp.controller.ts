b0808a96c29b67ba44bcc0159b8692531b8e28f7
hliebovkyrylo <kirilglebov06@gmail.com>
2025-04-29 16:30:58 +0300

    docs: documented otp

--- src/otp/otp.controller.ts
+++ src/otp/otp.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { OtpService } from './otp.service';
 import { AuthGuard } from '../common/guards/auth.guard';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('otp')
 export class OtpController {
@@ -8,6 +9,29 @@ export class OtpController {
 
   @Post('send')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: 'Create OTP',
+    description:
+      'Creates a one time password (OTP) for the user and sends it via email.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Otp was successfully generated and sent',
+    schema: {
+      type: 'object',
+      properties: {
+        message: { type: 'string', description: 'Message text' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Access token not provided or has expired',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Access token is not valid',
+  })
   async sendOtp(@Req() req) {
     const user = req.user;
     return this.otpService.generateAndSendOtp(user);
